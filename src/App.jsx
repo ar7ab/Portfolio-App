db406fc1c7580c1bc3731333ef01a0020d61c703
Ahmed Arhab <ahmedarhab018@gmail.com>
2024-11-06 17:47:38 +0200

    Upload App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DarkModeProvider } from './context/DarkModeContext.jsx';
-import Home from './pages/Home.jsx';
-import Products from './pages/Projects.jsx';
-import Resume from './pages/Resume.jsx';
-import Contact from './pages/Contact.jsx';
-
+import { Route, Routes } from 'react-router-dom'
+import { DarkModeProvider } from './context/DarkModeContext.jsx'
+import Home from './pages/Home.jsx'
+import Projects from './pages/Projects.jsx'
+import Resume from './pages/Resume.jsx'
+import Contact from './pages/Contact.jsx'
 
 const App = () => {
   return (
     <DarkModeProvider>
-      <Routes basename='/Portfolio-App/'>
+      <Routes basename='/ /'>
         <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<Products />} />
+        <Route path='/project' element={<Projects />} />
         <Route path='/resume' element={<Resume />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
     </DarkModeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
